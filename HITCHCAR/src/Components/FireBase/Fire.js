d4f4e6207f3bf8cacceaa0472ab2c7a84595d2c0
Ammar Ahmad <59066539+imammarahmad@users.noreply.github.com>
2024-07-25 12:23:14 +0500

    Update Fire.js

--- HITCHCAR/src/Components/FireBase/Fire.js
+++ HITCHCAR/src/Components/FireBase/Fire.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 import 'firebase/storage';
 var firebaseConfig = {
-    apiKey: "AIzaSyBeYbgumkY3-Yfzq3icwGd_U1DOoxay6GA",
+    apiKey: "",
     authDomain: "my-fyp-33a7f.firebaseapp.com",
     projectId: "my-fyp-33a7f",
     storageBucket: "my-fyp-33a7f.appspot.com",
-    messagingSenderId: "431427664806",
-    appId: "1:431427664806:web:d8b68cebb00f3470356d67",
-    measurementId: "G-ZKFVCRR95L"
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 // Initialize Firebase
 const fire = firebase.initializeApp(firebaseConfig);
